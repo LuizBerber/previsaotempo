f7194136aca352f4761d056a8bf6701bf39f5ecc
LuizBerber <luizberber@outlook.com>
2019-04-09 16:36:15 -0300

    adicionando classe que representa os dias da semana e criando relacionamento OneToOne com previsao de tempo

--- src/main/java/br/usjt/exer01/model/PrevisaoTempo.java
+++ src/main/java/br/usjt/exer01/model/PrevisaoTempo.java
@@ -5,16 +5,21 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
-public class PrevisaoTempo implements Serializable{
+public class PrevisaoTempo implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue
-	private long codigo;
-	
-	private String diaSemana;
+	private Long codigo;
+
+	@OneToOne
+	@JoinColumn (name = "diasemana")
+	private Dia diaSemana;
+
 	private double temperatura_minima;
 	private double temperatura_maxima;
 	private double humidade;
@@ -22,65 +27,106 @@ public class PrevisaoTempo implements Serializable{
 	private String latitude;
 	private String longitude;
 	private String data_hora;
-	
+
+	public Dia getDiaSemana() {
+		return diaSemana;
+	}
+
+	public void setDiaSemana(Dia diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+
 	public long getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
 	}
-	public String getDiaSemana() {
-		return diaSemana;
-	}
-	public void setDiaSemana(String diaSemana) {
-		this.diaSemana = diaSemana;
-	}
+
 	public double getHumidade() {
 		return humidade;
 	}
+
 	public void setHumidade(double humidade) {
 		this.humidade = humidade;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
 	public double getTemperatura_minima() {
 		return temperatura_minima;
 	}
+
 	public void setTemperatura_minima(double temperatura_minima) {
 		this.temperatura_minima = temperatura_minima;
 	}
+
 	public double getTemperatura_maxima() {
 		return temperatura_maxima;
 	}
+
 	public void setTemperatura_maxima(double temperatura_maxima) {
 		this.temperatura_maxima = temperatura_maxima;
 	}
+
 	public String getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
+
 	public String getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
+
 	public String getData_hora() {
 		return data_hora;
 	}
+
 	public void setData_hora(String data_hora) {
 		this.data_hora = data_hora;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
 
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final PrevisaoTempo other = (PrevisaoTempo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,13 @@
-insert into previsao_tempo (codigo, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao, latitude, longitude, data_hora) values (1, 'Segunda', 15.0, 21.0, 70.0, 'Faz frio', '22º 55´S', '22º 27´N', '02/12/1997 13h55');
-insert into previsao_tempo (codigo, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao, latitude, longitude, data_hora) values (2, 'terca', 14.0, 19.0, 30.0, 'Faz mais frio', '34º 65´S', '34º 69´N', '15/10/1999 11h11');
-insert into previsao_tempo (codigo, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao, latitude, longitude, data_hora) values (3, 'Quarta', 19.0, 31.0, 80.0, 'Esquentou', '29º 33´S', '29º 21´N', '08/01/2001 15h01');
+insert into dia (id, nome) values (1, 'domingo');
+insert into dia (id, nome) values (2, 'segunda');
+insert into dia (id, nome) values (3, 'terça');
+insert into dia (id, nome) values (4, 'quarta');
+insert into dia (id, nome) values (5, 'quinta');
+insert into dia (id, nome) values (6, 'sexta');
+insert into dia (id, nome) values (7, 'sábado');
+
+insert into previsao_tempo (codigo, diasemana, temperatura_minima, temperatura_maxima, humidade, descricao, latitude, longitude, data_hora) values (1, 1, 15.0, 21.0, 70.0, 'Faz frio', '22º 55´S', '22º 27´N', '02/12/1997 13h55');
+insert into previsao_tempo (codigo, diasemana, temperatura_minima, temperatura_maxima, humidade, descricao, latitude, longitude, data_hora) values (2, 3, 14.0, 19.0, 30.0, 'Faz mais frio', '34º 65´S', '34º 69´N', '15/10/1999 11h11');
+insert into previsao_tempo (codigo, diasemana, temperatura_minima, temperatura_maxima, humidade, descricao, latitude, longitude, data_hora) values (3, 1, 19.0, 31.0, 80.0, 'Esquentou', '29º 33´S', '29º 21´N', '08/01/2001 15h01');
 
 insert into usuario (id, login, senha) values (1, 'luizberber', '0099');
